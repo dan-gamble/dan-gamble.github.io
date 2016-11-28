921793ce1f5fd9facb56e892ff3043c68a3b387a
Dan Gamble <dangamble89@gmail.com>
2016-11-28 22:53:39 +0000

    Initial commit

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>Hi from {{ name }}</p>
+</template>
+
+<script>
+export default {
+  data ({ req }) {
+    return {
+      name: 'Dan Gamble'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome!</h1>
+    <router-link to="/about">About page</router-link>
+  </div>
+</template>
